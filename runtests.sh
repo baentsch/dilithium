2a98abec906448e6aa41434ea5263595106d667c
Michael Baentsch <mib@zurich.ibm.com>
2020-06-05 07:49:38 +0200

    further centos and OSX updates

--- runtests.sh
+++ runtests.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -e
 
-echo "Using CC = $CC"
-
 for dir in ref avx2; do
   make -C $dir
   for alg in 2 2aes 3 3aes 4 4aes; do
   ./$dir/test/test_vectors$alg > tvecs$alg
   ./$dir/PQCgenKAT_sign$alg
   done
-  sha256sum -c SHA256SUMS
+  # sha256sum not guaranteed to be installed in OSX
+  if [ `uname` == "Darwin" ]; then
+          shasum -a256 -c SHA256SUMS
+  else
+          sha256sum -c SHA256SUMS
+  fi
 done
